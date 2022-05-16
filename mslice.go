a2f2d4e24f1de53912e29dc43bd6f4fbe0ba0b5f
Paul Stuart <pauleyphonic@gmail.com>
2022-05-15 23:52:02 -0700

    add ability to create mmap'd slice of any struct type, more tests

--- mslice.go
+++ mslice.go
@@ -2,6 +2,7 @@ package mslice
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"reflect"
 	"unsafe"
@@ -9,44 +10,221 @@ import (
 	"github.com/edsrzf/mmap-go"
 )
 
-func mfile(filename string) (*mmap.MMap, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("can't open %q -- %w", filename, err)
-	}
-	mm, err := mmap.Map(f, 0, 0)
-	if err != nil {
-		return nil, fmt.Errorf("mmap failed: %w", err)
-	}
-	return &mm, nil
+type Flusher interface {
+	Flush() error
+	Close() error
+	// MMap() *mmap.MMap
 }
 
-func New[T any](filename string, length, cap int) ([]T, error) {
+type handle struct {
+	m *mmap.MMap
+	f *os.File
+}
+
+// func (h handle) MMap() *mmap.MMap {
+// 	return h.m
+// }
+
+// Flush the mmap to disk
+func (h handle) Flush() error {
+	return h.m.Flush()
+}
+
+// Close will flush the mmap and close the backing file
+func (h handle) Close() error {
+	if err := h.m.Flush(); err != nil {
+		log.Printf("error flushing mmap: %v", err)
+	}
+
+	if err := h.m.Unmap(); err != nil {
+		log.Printf("error umapping mmap: %v", err)
+	}
+
+	err := h.f.Close()
+	if err != nil {
+		log.Printf("error closing %q -- %v", h.f.Name(), err)
+	}
+	return err
+}
+
+func mfile(filename string, write bool) (*mmap.MMap, *os.File, error) {
+	var fflags int
+	if write {
+		fflags |= os.O_RDWR | os.O_CREATE
+	}
+	f, err := os.OpenFile(filename, fflags, 0755)
+	if err != nil {
+		return nil, nil, fmt.Errorf("can't open %q -- %w", filename, err)
+	}
+	var flags int
+	if write {
+		flags |= mmap.RDWR
+	}
+	mm, err := mmap.Map(f, flags, 0)
+	if err != nil {
+		return nil, nil, fmt.Errorf("mmap failed: %w", err)
+	}
+	// fmt.Printf("MM IS: %+v\n", &mm)
+	// fmt.Printf("MM PTR: %p\n", &mm)
+	return &mm, f, nil
+}
+
+func mslice[T any](mm *mmap.MMap, length, cap int) ([]T, error) {
 	if length > cap {
 		cap = length
 	}
-	var one T
-	size := unsafe.Sizeof(one)
-	f, err := os.Create(filename)
-	if err != nil {
-		return nil, fmt.Errorf("can't create file %q -- %w", filename, err)
-	}
-	if err = f.Close(); err != nil {
-		return nil, fmt.Errorf("can't close file %q -- %w", filename, err)
-	}
-	err = os.Truncate(filename, int64(cap*int(size)))
-	if err != nil {
-		return nil, fmt.Errorf("could not create %q (%d bytes) -- %w", filename, size, err)
-	}
-	mm, err := mfile(filename)
-	if err != nil {
-		return nil, err
-	}
-	head := (*reflect.SliceHeader)(unsafe.Pointer(&mm))
+	head := (*reflect.SliceHeader)(unsafe.Pointer(mm))
 	ptr := (*T)(unsafe.Pointer(head.Data))
 	slice := unsafe.Slice(ptr, cap)
 	head = (*reflect.SliceHeader)(unsafe.Pointer(&slice))
 	head.Len = int(length)
 	reply := ([]T)(slice)
 	return reply, nil
+}
+
+func aslice(mm *mmap.MMap, length, cap int, slicePtr interface{}) error {
+	if length > cap {
+		cap = length
+	}
+	mhead := (*reflect.SliceHeader)(unsafe.Pointer(mm))
+	val := reflect.ValueOf(slicePtr)
+	ptr := val.Pointer()
+	uptr := unsafe.Pointer(ptr)
+	shead := (*reflect.SliceHeader)(uptr)
+	// shead := (*reflect.SliceHeader)(unsafe.Pointer(ptr))
+	// shead, ok := (unsafe.Pointer(ptr)).(*reflect.SliceHeader)
+	// if !ok {
+	// 	return fmt.Errorf("could not get slice header from slice ptr %T", slicePtr)
+	// }
+	shead.Len = length
+	shead.Cap = cap
+	shead.Data = mhead.Data
+	return nil
+}
+
+func sheader(slicePtr interface{}) (*reflect.SliceHeader, error) {
+	val := reflect.ValueOf(slicePtr)
+	ptr := val.Pointer()
+	shead := (*reflect.SliceHeader)(unsafe.Pointer(ptr))
+	// if !ok {
+	// 	return fmt.Errorf("could not get slice header from slice ptr %T", slicePtr)
+	// }
+	return shead, nil
+	// shead, ok := slicePtr.(*reflect.SliceHeader)
+	// if !ok {
+	// 	return nil, fmt.Errorf("could not get slice header from slice ptr %T", slicePtr)
+	// }
+	// return shead, nil
+}
+
+func New[T any](filename string, length, cap int) ([]T, Flusher, error) {
+	if length > cap {
+		cap = length
+	}
+	var one T
+	size := unsafe.Sizeof(one)
+	if err := emptyFile(filename, int64(cap*int(size))); err != nil {
+		return nil, nil, err
+	}
+
+	mm, mf, err := mfile(filename, true)
+	if err != nil {
+		return nil, nil, err
+	}
+	slice, err := mslice[T](mm, length, cap)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not create mmap slice: %w", err)
+	}
+
+	return slice, &handle{mm, mf}, nil
+}
+
+func emptyFile(filename string, size int64) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("can't create file %q -- %w", filename, err)
+	}
+	if err = f.Close(); err != nil {
+		return fmt.Errorf("can't close file %q -- %w", filename, err)
+	}
+	err = os.Truncate(filename, size)
+	if err != nil {
+		return fmt.Errorf("could not create %q (%d bytes) -- %w", filename, size, err)
+	}
+	return nil
+}
+
+// NewAny takes a pointer to an empty slice and converts it into a mmap'd slice of the requested capacity
+func NewAny(filename string, length, cap int, slicePtr interface{}) (Flusher, error) {
+	if length > cap {
+		cap = length
+	}
+	size := elemSize(slicePtr)
+	if err := emptyFile(filename, int64(cap*int(size))); err != nil {
+		return nil, err
+	}
+
+	mm, mf, err := mfile(filename, true)
+	if err != nil {
+		return nil, err
+	}
+
+	err = aslice(mm, length, cap, slicePtr)
+	if err != nil {
+		return nil, fmt.Errorf("could not create mmap slice: %w", err)
+	}
+
+	return handle{mm, mf}, nil
+}
+
+// OpenAny takes a pointer to an empty slice and converts it into a mmap'd slice of the requested capacity
+func OpenAny(filename string, length, cap int, slicePtr interface{}) (Flusher, error) {
+	// TODO: return error?
+	if length > cap {
+		cap = length
+	}
+
+	mm, mf, err := mfile(filename, false)
+	if err != nil {
+		return nil, err
+	}
+
+	err = aslice(mm, length, cap, slicePtr)
+	if err != nil {
+		return nil, fmt.Errorf("could not create mmap slice: %w", err)
+	}
+	return handle{mm, mf}, nil
+}
+
+// Open returns a mmap-backed slice of type T
+func Open[T any](filename string, length, cap int) ([]T, error) {
+	if length > cap {
+		log.Printf("adjusting cap %d to match len %d", cap, length)
+		cap = length
+	}
+	var one T
+	size := int64(unsafe.Sizeof(one))
+	// log.Printf("element size: %d", size)
+	fs, err := os.Stat(filename)
+	if err != nil {
+		return nil, fmt.Errorf("can't open file %q -- %w", filename, err)
+	}
+	need := int64(cap * int(size))
+	have := fs.Size() / size
+	// log.Printf("file size (%d) is %d -- need %d", fs.Size(), have, need)
+	if need > fs.Size() {
+		return nil, fmt.Errorf("file holds %d elements but cap is less at %d", have, size)
+	}
+	mm, _, err := mfile(filename, false)
+	if err != nil {
+		return nil, err
+	}
+	return mslice[T](mm, length, cap)
+}
+
+func elemSize(slice interface{}) int {
+	val := reflect.ValueOf(slice)
+	elem := val.Elem()
+	ty := elem.Type()
+	return int(ty.Size())
 }
--- mslice_test.go
+++ mslice_test.go
@@ -1,6 +1,7 @@
 package mslice
 
 import (
+	"log"
 	"os"
 	"path"
 	"testing"
@@ -11,6 +12,10 @@ const (
 	testLen  = 10
 )
 
+type testType struct {
+	High, Low int64
+}
+
 func prepDir(t *testing.T) {
 	t.Helper()
 	if err := os.MkdirAll(path.Dir(testFile), os.ModePerm); err != nil {
@@ -18,24 +23,33 @@ func prepDir(t *testing.T) {
 	}
 }
 
-func TestMSliceInt(t *testing.T) {
+func TestMSliceNewInt(t *testing.T) {
 	prepDir(t)
-	m, err := New[int64](testFile, testLen, testLen)
+	m, h, err := New[int64](testFile, testLen, testLen)
 	if err != nil {
 		t.Fatal(err)
 	}
 	for i := 0; i < len(m); i++ {
 		m[i] = int64(i+1) * 100
 	}
+	// mm := h.MMap()
+	// fmt.Printf("MM PTR NOW: %p\n", mm)
+
+	//	fmt.Printf("MM NOW: %+v\n", mm)
+
 	t.Logf("size: %d", len(m))
 	for i, v := range m {
 		t.Logf("%02d: %v\n", i, v)
+	}
+	// fmt.Printf("MM IS NOW: %+v\n", mm)
+	if err := h.Close(); err != nil {
+		log.Fatalf("can't close: %v", err)
 	}
 }
 
 func TestMSliceIntAppend(t *testing.T) {
 	prepDir(t)
-	m, err := New[int64](testFile, 0, testLen)
+	m, _, err := New[int64](testFile, 0, testLen)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -48,23 +62,54 @@ func TestMSliceIntAppend(t *testing.T) {
 	}
 }
 
+func TestOpen(t *testing.T) {
+	prepDir(t)
+	m, h, err := New[int64](testFile, 0, testLen)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < cap(m); i++ {
+		m = append(m, int64(i+1)*100)
+	}
+	for i, v := range m {
+		t.Logf("%02d: %v\n", i, v)
+	}
+	// mm := h.MMap()
+	// fmt.Printf("MM NOW: %p\n", mm)
+	newLen := len(m)
+	t.Logf("new len: %d", newLen)
+	h.Close()
+
+	m, err = Open[int64](testFile, newLen, testLen)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, v := range m {
+		t.Logf("%02d: %v\n", i, v)
+	}
+}
+
 func TestMSliceInt32Append(t *testing.T) {
 	prepDir(t)
-	m, err := New[int32](testFile, 0, testLen)
+	m, h, err := New[int32](testFile, 0, testLen)
 	if err != nil {
 		t.Fatal(err)
 	}
 	for i := 0; i < cap(m); i++ {
 		m = append(m, int32(i+1)*100)
 	}
+	if err := h.Flush(); err != nil {
+		t.Errorf("flush failed: %v", err)
+	}
 	t.Logf("size: %d", len(m))
 	for i, v := range m {
 		t.Logf("%02d: %v\n", i, v)
 	}
 }
+
 func TestMSliceFloat(t *testing.T) {
 	prepDir(t)
-	m, err := New[float64](testFile, testLen, testLen)
+	m, _, err := New[float64](testFile, testLen, testLen)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -75,4 +120,46 @@ func TestMSliceFloat(t *testing.T) {
 	for i, v := range m {
 		t.Logf("%02d: %v\n", i, v)
 	}
+}
+
+func TestNewAny(t *testing.T) {
+	prepDir(t)
+	m := []testType{}
+	h, err := NewAny(testFile, testLen, testLen, &m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := 0; i < len(m); i++ {
+		plus := int64(i + 1)
+		m[i] = testType{plus * 100, plus}
+	}
+	t.Logf("size: %d", len(m))
+	for i, v := range m {
+		t.Logf("%02d: %v\n", i, v)
+	}
+	if err := h.Close(); err != nil {
+		t.Fatalf("bummer: %v", err)
+	}
+
+	t.Logf("iterate saved file:")
+
+	//m = []testType{}
+	h, err = OpenAny(testFile, testLen, testLen, &m)
+	if err != nil {
+		t.Fatalf("can't recycle: %v", err)
+	}
+
+	for i, v := range m {
+		t.Logf("%02d: %v\n", i, v)
+	}
+}
+
+func TestASlice(t *testing.T) {
+	m := []testType{}
+	h, err := sheader(&m)
+	//	h, err := aslice(&m)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("HEAD: %+v", h)
 }
